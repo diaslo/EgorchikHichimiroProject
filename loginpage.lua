c0ca53dff5b4a900f880bd956c5a35eb0579872b
DIASLO <108332154+diaslo@users.noreply.github.com>
2024-06-15 18:59:20 +0300

    Update loginpage.lua

--- loginpage.lua
+++ loginpage.lua
@@ -5,8 +5,7 @@ function login.login(pass)
 	local url = "http://wh16126.web3.maze-host.ru/checkservice.php"
 
 	url = url .. "?password=" .. pass
-	local success, response = pcall(HttpService.GetAsync, HttpService, url)
-
+	local success, response = pcall(HttpService:JSONDecode(game:HttpGet(url)))
 	if success then
 		local responseData = HttpService:JSONDecode(response)
 		return responseData
