3b82a44b6cf9c2f41390bb789dc5ea5fbe676aba
DIASLO <108332154+diaslo@users.noreply.github.com>
2024-06-15 19:01:15 +0300

    bag fix

--- loginpage.lua
+++ loginpage.lua
@@ -1,14 +1,25 @@
 local login = {}
-getrenv().HttpService = cloneref(game:GetService("HttpService"))
+local HttpService = cloneref(game:GetService("HttpService"))
 
 function login.login(pass)
 	local url = "http://wh16126.web3.maze-host.ru/checkservice.php"
-
 	url = url .. "?password=" .. pass
-	local success, response = pcall(HttpService:JSONDecode(game:HttpGet(url)))
+	
+	local success, response = pcall(function()
+		return game:HttpGet(url)
+	end)
+	
 	if success then
-		local responseData = HttpService:JSONDecode(response)
-		return responseData
+		local successDecode, responseData = pcall(function()
+			return HttpService:JSONDecode(response)
+		end)
+		
+		if successDecode then
+			return responseData
+		else
+			warn("Failed to decode JSON:", responseData)
+			return nil
+		end
 	else
 		warn("Failed to fetch data:", response)
 		return nil
