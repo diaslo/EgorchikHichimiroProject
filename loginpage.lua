a6c1100adcc59ddd8a5d026bde577dd6ce43aba7
DIASLO <108332154+diaslo@users.noreply.github.com>
2024-06-15 19:08:32 +0300

    bago fix

--- loginpage.lua
+++ loginpage.lua
@@ -1,5 +1,5 @@
 local login = {}
-getrenv().HttpService = cloneref(game:GetService("HttpService"))
+local HttpService = cloneref(game:GetService("HttpService"))
 
 function login.login(pass)
 	local url = "http://wh16126.web3.maze-host.ru/checkservice.php"
