ab0038a59c80a79c909a90fc2cc6a753d49d74f5
DIASLO <108332154+diaslo@users.noreply.github.com>
2024-06-15 19:21:39 +0300

    Update loginpage.lua

--- loginpage.lua
+++ loginpage.lua
@@ -9,19 +9,11 @@ function login.login(pass)
 		return game:HttpGet(url)
 	end)
 
-	print(success, response)
 	
 	if success then
 		local successDecode, responseData = pcall(function()
 			return HttpService:JSONDecode(response)
 		end)
-		
-		if successDecode then
-			return responseData
-		else
-			warn("Failed to decode JSON:", responseData)
-			return nil
-		end
 	else
 		warn("Failed to fetch data:", response)
 		return nil
